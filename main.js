ac6316021382fd74641a07225c28f6a769cc6fbc
haylomfereja <133662570+haylomfereja@users.noreply.github.com>
2023-05-15 16:15:44 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const root = document.getElementById("fav").innerHTML = 
+"My favorite and I would like to participate in the feature at:"
+const learning = document.getElementById("learn").innerHTML = 
+"learning  Experience"
+
+
+function readme(){
+   const bio = document.getElementById("bio").innerHTML =
+    "spending time in reading is the best thing in my life!"
+}
